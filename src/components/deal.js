b4c971987f1ad6f98b6d81751434892093d0dd09
Oscar Dobson-Brown <oscardobsonbrown@outlook.com>
2023-09-12 16:01:12 +0800

    fixed a spacing issue

--- src/components/deal.js
+++ src/components/deal.js
@@ -5,25 +5,26 @@ function Deals() {
 	// using map we can loop through each pair in the relevant object to get each unique component
 	const aDeal = deals.map((deals) => (
 		<li className="col-md-12">
-			<div className="image">
-				{/* might need to change the image to a source in data.js but this works for now */}
-				<a href="blog-detail.html">
+			<a href="blog-detail.html">
+				<div className="image">
+					{/* might need to change the image to a source in data.js but this works for now */}
+
 					<img alt="" src="http://placehold.it/100x100" />
-				</a>
-			</div>
-			<ul className="top-info">
-				<li>
-					<i className="fa fa-calendar" />
-					{/* get the availability from the data file */}
-					Available {deals.availability}
-				</li>
-			</ul>
-			<h4>
-				{/* get the name of the deal from the data file */}
-				<a href="blog-detail.html">{deals.title}</a>
-				{/* had to change from p to label to make w3cvalidator happy and play with some css but this looks identical and is correct */}
-				<label>{deals.region}</label>
-			</h4>
+				</div>
+				<div className="top-info">
+					<p>
+						<i className="fa fa-calendar" />
+						{/* get the availability from the data file */}
+						Available {deals.availability}
+					</p>
+				</div>
+				<h4>
+					{/* get the name of the deal from the data file */}
+					<a href="blog-detail.html">{deals.title}</a>
+					{/* had to change from p to label to make w3cvalidator happy and play with some css but this looks identical and is correct */}
+					<label>{deals.region}</label>
+				</h4>
+			</a>
 		</li>
 	));
 	// render each deal
